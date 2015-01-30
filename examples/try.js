6589622a8b7eac038583d2e52adcd75b3aeb6060
foliveira <fabio.an.oliveira@gmail.com>
2015-01-30 17:45:50 +0100

    Corrected examples

--- examples/try.js
+++ examples/try.js
@@ -1,6 +1,6 @@
 var bacon = require('./../src/');
 
-bacon.start('4040', function gotBacon(port) {
-    console.log('bacon', port);
-
+bacon.start('4040', function gotBacon(address, port) {
+    console.log('bacon address', address);
+    console.log('bacon port', port);
 });
